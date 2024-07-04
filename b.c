cc1dfbbbcf4609b5d71a4a450db79955d9431f6c
AKABANE Ryuga <s1310067@u-aizu.ac.jp>
2024-07-04 13:01:42 +0900

    User name added

--- b.c
+++ b.c
@@ -4,7 +4,12 @@
 
 int main() {
     int i, heads_count = 0, tails_count = 0;
+    char user_name[100];  
 
+    printf("Who are you？\n> ");
+    scanf("%s", user_name);
+    
+    printf("Hello, %s!\n", user_name);
     printf("Tossing a coin...\n");
     for (i = 1; i <= 3; i++) {
         int result = rand() % 2;
